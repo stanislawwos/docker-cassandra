363cefc3df0a95a9c8bdff99d915c03c114041a7
Dawid Malinowski <dawidmalina@gmail.com>
2015-10-29 22:35:11 +0100

    Bump to latest changes

--- server/start-cassandra.sh
+++ server/start-cassandra.sh
@@ -1,44 +1,44 @@
-#!/bin/sh
+#!/bin/bash
 
-if [[ $DC_NAME && $RACK_NAME ]]; then
+if [ "$DC_NAME" != "" ] && [ "$RACK_NAME" != "" ]; then
   sed -i -e "s/dc=DC1/dc=${DC_NAME}/" /etc/cassandra/cassandra-rackdc.properties
   sed -i -e "s/rack=RAC1/rack=${DC_NAME}/" /etc/cassandra/cassandra-rackdc.properties
   sed -i -e 's/^# prefer_local=true/prefer_local=true/' /etc/cassandra/cassandra-rackdc.properties
 fi
 
-if [[ $SNITCH ]]; then
+if [ "$SNITCH" != "" ]; then
   sed -i -e "s/endpoint_snitch: SimpleSnitch/endpoint_snitch: $SNITCH/" /etc/cassandra/cassandra.yaml
 fi
 
-if [[ $OPSCENTER_IP ]]; then
+if [ "$OPSCENTER_IP" != "" ]; then
   echo -e "stomp_interface: ${$OPSCENTER_IP}" >> /var/lib/datastax-agent/conf/address.yaml
 fi
 
-if [[ $OPSCENTER_USE_SSL ]]; then
+if [ "$OPSCENTER_USE_SSL" != "" ]; then
   echo -e "use_ssl: ${OPSCENTER_USE_SSL}" >> /var/lib/datastax-agent/conf/address.yaml
 fi
 
-if [[ $CLUSTER_NAME ]]; then
+if [ "$CLUSTER_NAME" != "" ]; then
   sed -i -e "s/cluster_name: 'Test Cluster'/cluster_name: '${CLUSTER_NAME}'/" /etc/cassandra/cassandra.yaml
 fi
 
 for SEED in $( env |grep SEED_ |sort |awk 'match($0, /SEED_[0-9]+/) { print substr( $0, RSTART, RLENGTH )}' |uniq )
 do
-  CURRENT==$( env |grep ${SEED} |sed 's/^[^=]*=//' )
-  SEEDS_LIST="$SEEDS $CURRENT"
+  CURRENT=$( env |grep ${SEED} |sed 's/^[^=]*=//' )
+  SEEDS_LIST="$SEEDS_LIST $CURRENT"
 done
 
-if [[ $SEEDS_LIST ]]; then
-  SEEDS=$( echo ${SEEDS} | tr ' ' ', ' )
-  sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: \${SEEDS}\"/" /etc/cassandra/cassandra.yaml
+if [ "$SEEDS_LIST" != "" ]; then
+  SEEDS=$( echo ${SEEDS_LIST} | tr ' ' ', ' )
+  sed -i -e "s/- seeds: \"127.0.0.1\"/- seeds: ${SEEDS}\"/" /etc/cassandra/cassandra.yaml
 fi
 
-if [[ $BROADCAST_ADDRESS ]]; then
+if [ "$BROADCAST_ADDRESS" != "" ]; then
   echo -e "local_interface: ${BROADCAST_ADDRESS}" >> /var/lib/datastax-agent/conf/address.yaml
   sed -i -e "s/# broadcast_address: 1.2.3.4/broadcast_address: ${BROADCAST_ADDRESS}/" /etc/cassandra/cassandra.yaml
 fi
 
-if [[ $BROADCAST_RPC ]]; then
+if [ "$BROADCAST_RPC" != "" ]; then
   sed -i -e "s/# broadcast_rpc_address: 1.2.3.4/broadcast_rpc_address: ${BROADCAST_RPC}/" /etc/cassandra/cassandra.yaml
 fi
 
